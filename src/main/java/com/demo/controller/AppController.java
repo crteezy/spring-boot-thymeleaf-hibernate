92fc47271fd2ce672140e3837f42c737a428947c
Chamithra Thenuwara <43117071+crteezy@users.noreply.github.com>
2019-12-27 15:48:50 +0530

    changes

--- src/main/java/com/demo/controller/AppController.java
+++ src/main/java/com/demo/controller/AppController.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.demo.domain.Product;
 import com.demo.service.ProductService;
@@ -17,17 +18,41 @@ public class AppController {
 	@Autowired
 	private ProductService service;
 	
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String displayViewProductsPage(Model model) {
+//		List<Product> products = service.getAllProducts();
+//		model.addAttribute("products", products);
+//		return "/html/index";
+//	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String displayViewProductsPage(Model model) {
+	public ModelAndView displayViewProductsPage() {
 		List<Product> products = service.getAllProducts();
-		model.addAttribute("products", products);
-		return "/html/index";
+		ModelAndView view = new ModelAndView("/html/index");
+		view.addObject("products", products);
+		return view;
 	}
 	
+//	@RequestMapping(value = "/addNew", method = RequestMethod.GET)
+//	public String displayCreateProductsPage(Model model) {
+//		Product product = new Product();
+//		model.addAttribute("product", product);
+//		return "/html/new_product";
+//	}
+	
 	@RequestMapping(value = "/addNew", method = RequestMethod.GET)
-	public String displayCreateProductsPage(Model model) {
+	public ModelAndView displayCreateProductsPage() {
+		ModelAndView view = new ModelAndView("/html/new_product");
 		Product product = new Product();
-		model.addAttribute("product", product);
-		return "/html/new_product";
+		view.addObject("product", product);
+		return view;
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView displayEditProductsPage(@PathVariable(name = "id") Long id) {
+		ModelAndView view = new ModelAndView("/html/edit_product");
+		Product product = service.getProduct(id);
+		view.addObject("product", product);
+		return view;
 	}
 }
--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +19,12 @@ public class ProductController {
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveProduct(@ModelAttribute Product product) {
 		service.saveProduct(product);
-		return "redirect:/addNew";
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteProduct(@PathVariable(name = "id") Long id) {
+		service.deleteProduct(id);
+		return "redirect:/";
 	}
 }
